2e3918c3c3cf1890c6af0b6ffda825b5a4adf338
Tim Lander <tim57282@hotmail.com>
2019-05-23 13:58:45 +0800

    Revert "Enable rtti + dylib tests with wasm backend (#8510)"
    This reverts commit da96266d3ecbcdf3498bf2cedfde83a0db2eaa51.

--- tests/test_core.py
+++ tests/test_core.py
@@ -4142,7 +4142,7 @@ ok
   #                              open(path_from_root('tests', 'bullet', 'output3.txt')).read()])
 
   @needs_dlfcn
-  @no_fastcomp('https://github.com/emscripten-core/emscripten/issues/8376')
+  @no_wasm_backend('current fails in __dynamic_cast')
   def test_dylink_rtti(self):
     # Verify that objects created in one module and be dynamic_cast<> correctly
     # in the another module.
@@ -4187,10 +4187,12 @@ ok
     }
     '''
 
+    # TODO(sbc): This tests should instead return zero.  Update expectations
+    # once we fix https://github.com/emscripten-core/emscripten/issues/8376
     self.dylink_test(main=main,
                      side=side,
                      header=header,
-                     expected='success')
+                     expected='failure')
 
   def test_random(self):
     src = r'''#include <stdlib.h>
@@ -4757,6 +4759,7 @@ name: .
     Building.COMPILER_TEST_OPTS += ['--embed-file', path_from_root('tests/utf16_corpus.txt') + '@/utf16_corpus.txt']
     self.do_run(open(path_from_root('tests', 'benchmark_utf16.cpp')).read(), 'OK.')
 
+  @no_wasm_backend('printf is incorrectly handling float values')
   def test_wprintf(self):
     self.do_run_in_out_file_test('tests', 'core', 'test_wprintf')
 
@@ -5134,6 +5137,7 @@ PORT: 3979
     expected = open(path_from_root('tests', 'netinet', 'in.out')).read()
     self.do_run(src, expected)
 
+  @no_wasm_backend('No dynamic linking support in wasm backend path')
   def test_main_module_static_align(self):
     if self.get_setting('ALLOW_MEMORY_GROWTH'):
       self.skipTest('no shared modules with memory growth')
